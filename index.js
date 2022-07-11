cc82d7aac7eb8c20003083db358e552551cfcdd0
Carlos Rivera <103550985+c83rivera@users.noreply.github.com>
2022-07-10 22:15:35 -0500

    CR
    CR

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+let myFunction = [1, 3, 5, 7, 10, 11];
+
+for(let i= 0;i<myFunction.length;i++){
+    if(myFunction[i]%2==0){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+}
+
+let myFunction = [1, 3, 5];
+
+for(let i= 0;i<myFunction.length;i++){
+    if(myFunction[i]%2==0){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+}
+
+
+
+
+
+for(i=0; i<41; i++){
+    console.log("4*"+[i]+" = "+4*[i]);
+}
